0f70babc9c63489354ee8fdb9aa3140fda65498c
Azanul <azanulhaque@gmail.com>
2024-02-08 13:30:26 +0530

    feat: user proto conversion
    Signed-off-by: Azanul <azanulhaque@gmail.com>

---
+++ user/pkg/model/proto.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"wuphf.com/user/gen"
+)
+
+// MetadataToProto converts a Metadata struct into a generated proto counterpart.
+func UserToProto(m *User) *gen.User {
+	return &gen.User{
+		Id:    m.ID,
+		Email: m.Email,
+	}
+}
+
+// MetadataFromProto converts a generated proto counterpart into a Metadata struct.
+func UserFromProto(m *gen.User) *User {
+	return &User{
+		ID:    m.Id,
+		Email: m.Email,
+	}
+}
