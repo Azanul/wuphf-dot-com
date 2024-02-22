9d31c8eb087ef58d4d35444320aba26ee900e0db
Azanul <azanulhaque@gmail.com>
2024-02-22 15:16:28 +0530

    feat: notification migrations
    Signed-off-by: Azanul <azanulhaque@gmail.com>

---
+++ notification/pkg/model/migration/notification_1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE notifications (
+    id SERIAL PRIMARY KEY,
+    chat_id VARCHAR(255),
+    sender VARCHAR(255),
+    receiver VARCHAR(255),
+    msg TEXT
+);
---
+++ notification/pkg/model/migration/notification_2.sql
@@ -0,0 +1,2 @@
+ALTER TABLE notifications
+ADD COLUMN reference TEXT;
